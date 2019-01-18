27b5c24a67dff35f8ec4fff601fca5d7f562aa61
Pavel Agarkov <pavel.agarkov@gmail.com>
2019-01-18 11:19:14 +0300

    line break fix

--- shell/tls/10-update-helm.sh
+++ shell/tls/10-update-helm.sh
@@ -1,7 +1,6 @@
 kubectl create serviceaccount tiller --namespace=kube-system;
 
-kubectl create clusterrolebinding tiller-admin --serviceaccount=kube-system:tiller --clus
-terrole=cluster-admin;
+kubectl create clusterrolebinding tiller-admin --serviceaccount=kube-system:tiller --clusterrole=cluster-admin;
 
 helm init --upgrade --service-account=tiller;
 
